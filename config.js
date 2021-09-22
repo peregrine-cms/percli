164b95e6edd1921b232cc0a4bc01c4c4648d600d
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2021-09-22 14:39:37 -0700

    Added initial configuration support and basic Sling startup

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  download: {
+    url: 'https://raw.githubusercontent.com/wiki/peregrine-cms/percli/percli-server-install-script.md',
+    dir: 'out',
+  },
+  sling: {
+    dir: 'sling'
+  }
+}
+
+module.exports = config
