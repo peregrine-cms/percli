e9494ddd0c2f3287a00c85fadd179588658858c1
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2021-10-08 11:27:20 -0700

    Updated percli to use a FAR

--- config.js
+++ config.js
@@ -2,6 +2,10 @@ const os = require('os')
 const path = require('path')
 
 const config = {
+  kickstarter: {
+    jar: 'org.apache.sling.kickstart-0.0.12.jar',
+    far: 'com.peregrine-cms.sling.launchpad-12-SNAPSHOT-oak_tar_fds_far.far'
+  },
   download: {
     url: 'https://raw.githubusercontent.com/wiki/peregrine-cms/percli/percli-server-install-script-for-kickstarter.md',
     dir: 'out',
